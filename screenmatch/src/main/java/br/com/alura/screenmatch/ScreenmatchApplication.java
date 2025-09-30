1020b041fd951933a9e3568d0de82f07e37664a5
Vladimir Monteiro <vladimir.monteiro021@gmail.com>
2025-09-30 20:51:13 -0300

    Pragicando streams

--- screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,14 +1,10 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.models.DadosSerie;
+import br.com.alura.screenmatch.principal.Principal;
 import br.com.alura.screenmatch.services.ConsumoApi;
-import br.com.alura.screenmatch.services.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.net.URL;
-import java.net.URLEncoder;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -20,9 +16,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 	var json = ConsumoApi.consumoApi("http://www.omdbapi.com/?t=matrix&apikey=e027d2e4");
-
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
--- screenmatch/src/main/java/br/com/alura/screenmatch/models/DadosTemporada.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/models/DadosTemporada.java
@@ -3,9 +3,9 @@ package br.com.alura.screenmatch.models;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DadosSerie(
-        @JsonAlias("Title") String titulo,
-        @JsonAlias("totalSeasons") int totalTemporadas,
-        @JsonAlias("imdbRating") String avaliacao) {
+public record DadosTemporada(@JsonAlias("Season") Integer numero,
+                             @JsonAlias("Episodes")List<DadosEpisodio> episodios) {
 }
